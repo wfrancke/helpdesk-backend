ff85ddc0fd7fe95b7d9b058188281da3c6655c6f
Wojciech Francke <wojfra99@gmail.com>
2021-12-12 18:49:21 +0100

    frontend connectivity fixes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -21,7 +21,7 @@ export class AuthService {
   async login(user: User) {
     const payload = { email: user.email, sub: user._id };
     return {
-      access_token: this.jwtService.sign(payload),
+      accessToken: this.jwtService.sign(payload),
     };
   }
 }
